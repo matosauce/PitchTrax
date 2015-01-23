9024a296fd30a9ecaa937ad63262c0fb18ce085f
Sweetness23 <koenigjoey@gmail.com>
2015-01-23 10:32:51 -0600

    Add R Squared Calculator

--- Statistics/LineFitting.cs
+++ Statistics/LineFitting.cs
@@ -1,4 +1,7 @@
-﻿using MathNet.Numerics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics;
 
 namespace Statistics
 {
@@ -10,6 +13,21 @@ namespace Statistics
             if (order != 1) return Fit.Polynomial(x, y, order);
             var data =  Fit.Line(x, y);
             return new[]{data.Item1, data.Item2};
+        }
+
+        public double RSquared(double[] equation, double[] xValues, double[] yValues)
+        {
+            var equationValues = new List<double>();
+            foreach (var value in xValues)
+            {
+                double equationValue = 0;
+                for (var i = 0; i < equation.Count(); i ++)
+                {
+                    equationValue += equation[i]*(Math.Pow(value,i));
+                }
+                equationValues.Add(equationValue);
+            }
+            return GoodnessOfFit.RSquared(equationValues, yValues);
         }
     }
 }
